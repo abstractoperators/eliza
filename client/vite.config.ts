e1f123f3c833559e3b47ff8b2cbd9c80fe17cc9f
Dance <dance@abstractoperators.ai>
2025-02-07 14:37:04 -0800

    removed brotli compression from client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        viteCompression({
-            algorithm: "brotliCompress",
-            ext: ".br",
-            threshold: 1024,
-        }),
     ],
     clearScreen: false,
     build: {
