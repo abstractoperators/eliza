54823f317bc891ade1db954499dff13b8ca1765e
Michael Deng <michaeldeng2003@gmail.com>
2025-02-27 18:03:05 -0600

    Remove debug log + corresponding import

--- packages/core/src/instrumentation.js
+++ packages/core/src/instrumentation.js
@@ -1,12 +1,6 @@
 import { registerOTel } from "@vercel/otel";
-import { diag, DiagLogLevel, DiagConsoleLogger } from '@opentelemetry/api';
 
 export function register() {
-    // Load environment variables
-    
-    // Enable OpenTelemetry debug logging
-    diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
-    
     try {
         console.log("Registering OpenTelemetry...");
         registerOTel("Eliza");
